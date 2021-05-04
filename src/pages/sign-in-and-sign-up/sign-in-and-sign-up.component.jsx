7b932c23b21c651053215cf601c4502149c0535b
moyoakin <moyoakin1211@gmail.com>
2021-05-03 20:06:26 -0700

    complete implementation

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import "./sign-in-and-sign-up.styles.css";
 
 import SignIn from "../../components/sign-in/sign-in.component";
-import SignUp from '../../components/sign-up/sign-up.component';
+import SignUp from "../../components/sign-up/sign-up.component";
 
-const SignInAndSignUpPage = ({history}) => (
+const SignInAndSignUpPage = ({ history }) => (
   <div className="sign-in-and-sign-up">
     <SignIn history={history} />
     <SignUp />
